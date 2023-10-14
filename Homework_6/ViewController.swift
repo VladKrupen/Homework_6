da305f18c13bdebc6ca881a35be0b32cc56d8c78
Vlad Krupen <air7360@gmail.com>
2023-10-14 20:22:03 +0300

    Initial commit

---
+++ Homework_6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_6
+//
+//  Created by Vlad on 14.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
